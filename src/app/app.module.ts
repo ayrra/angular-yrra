0ab68c36fe49bc6fb7de0d5185e11cfcd87b717c
Andres Yrra <andy@yrra.net>
2017-09-03 22:22:59 -0700

    added a few components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MaterialModule} from '@angular/material';
+import {FlexLayoutModule} from '@angular/flex-layout';
+
 import { AppComponent } from './app.component';
+import { MainmenuComponent } from './mainmenu/mainmenu.component';
+import { ContentComponent } from './content/content.component';
+
+import 'hammerjs';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainmenuComponent,
+    ContentComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+    FlexLayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
