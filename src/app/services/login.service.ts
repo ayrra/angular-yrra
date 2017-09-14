f355b51bbb4d27e23fa8ddb7564fc6673a94adf6
Andres Yrra <andy@yrra.net>
2017-09-13 19:19:16 -0700

    now a session based login service

--- src/app/services/login.service.ts
+++ src/app/services/login.service.ts
@@ -8,20 +8,18 @@ import {Observable} from 'rxjs/Observable';
 @Injectable()
 export class LoginService {
 
-  token: string;
-
   constructor(private restangular: Restangular) { }
 
   requestAuth(creds: Credentials): Observable<Credentials> {
-    return this.restangular.all('users/login').post(creds).subscribe(key => {this.token = key.token;});
+    return this.restangular.all('users/login').post(creds).subscribe(key => {window.sessionStorage.setItem('login-token', key.token);});
   }
 
   checkAuth(): string {
-    return this.token;
+    return window.sessionStorage.getItem('login-token');
   }
 
   destroyToken() {
-    this.token = undefined;
+    window.sessionStorage.setItem('login-token', '');
   }
 
 }
